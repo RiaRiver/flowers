f8c18a5c38782bb44a52138c64e7034625ebe4f2
RiaRiver <riria.river@gmail.com>
2020-11-12 15:45:33 +0300

    lesson06: add order according to video

---
+++ src/Components/Order.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import styled from 'styled-components';
+import Button from "./Button";
+import OrderListItem from "./OrderListItem";
+
+const OrderStyled = styled.section`
+  position: fixed;
+  top: 80px;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  
+  background: white;
+  min-width: 360px;
+  height: calc(100% - 80px);
+  padding: 24px;
+  box-shadow: 3px 4px 5px rgba(0, 0, 0, 0.25);
+  z-index: 99;
+`;
+
+const OrderTitle = styled.h2`
+ text-align: center;
+ text-transform: uppercase;
+`;
+
+const OrderContent = styled.div`
+ flex-grow: 1;
+`;
+
+const OrderList = styled.ul`
+
+`;
+
+const Total = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+  & span:first-child{
+  flex-grow: 1;
+  }
+`;
+
+const TotalPrice = styled.span`
+  margin: 0 34px 0 20px ;
+  min-width: 100px;
+  text-align: right;
+`;
+
+const Order = () => (
+  <>
+    <OrderStyled>
+      <OrderTitle>Your order</OrderTitle>
+      <OrderContent>
+        <OrderList>
+          <OrderListItem/>
+          <OrderListItem/>
+          <OrderListItem/>
+        </OrderList>
+      </OrderContent>
+      <Total>
+        <span>Total</span>
+        <span>5</span>
+        <TotalPrice>{(525).toLocaleString('en-En',
+          { style: 'currency', currency: 'USD' })}</TotalPrice>
+      </Total>
+      <Button>Make an order</Button>
+    </OrderStyled>
+  </>
+);
+
+export default Order;
---
+++ src/Components/OrderListItem.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+import trashImage from '../images/trash.svg';
+
+const OrderItemStyled = styled.li`
+  display: flex;
+  margin: 15px 0;
+`;
+
+const ItemName = styled.span`
+  flex-grow: 1;
+`;
+
+const ItemPrice = styled.span`
+  margin: 0 10px 0 20px ;
+  min-width: 100px;
+  text-align: right;
+`;
+
+
+const TrashButton = styled.button`
+  width: 24px;
+  height: 24px;
+  border-color: transparent;
+  background-color: transparent;
+  background-image: url(${trashImage});
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+`;
+
+const OrderListItem = () => (
+  <OrderItemStyled>
+    <ItemName>Name</ItemName>
+    <span>2</span>
+    <ItemPrice>{(200).toLocaleString('en-En',
+      { style: 'currency', currency: 'USD' })}</ItemPrice>
+    <TrashButton/>
+  </OrderItemStyled>
+);
+
+export default OrderListItem;
